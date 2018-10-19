7a34e1f72a68753428776b7fec4f7c220db61552
Jeremy Cui <tsuijy@gmail.com>
2018-10-19 17:30:42 +0800

    Remove unused codes

--- src/main/java/com/genesisfin/backend/web/domain/ApiFactory.java
+++ src/main/java/com/genesisfin/backend/web/domain/ApiFactory.java
@@ -1,10 +1,6 @@
 package com.genesisfin.backend.web.domain;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static com.genesisfin.backend.web.domain.Api.createApi;
 
 @Configuration
 public class ApiFactory {
--- src/main/java/com/genesisfin/backend/web/viewmodel/PagedResult.java
+++ src/main/java/com/genesisfin/backend/web/viewmodel/PagedResult.java
@@ -2,7 +2,6 @@ package com.genesisfin.backend.web.viewmodel;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.domain.Page;
 
 import java.util.List;
 
