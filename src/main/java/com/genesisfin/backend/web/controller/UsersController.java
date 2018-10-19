7ec3d3a4abb4c02c6128b154eac73b55b2fcb1a6
Jeremy Cui <tsuijy@gmail.com>
2018-10-19 12:08:25 +0800

    Add  the show, update, destroy actions.

--- src/main/java/com/genesisfin/backend/web/controller/UsersController.java
+++ src/main/java/com/genesisfin/backend/web/controller/UsersController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -22,18 +24,57 @@ public class UsersController {
     @GetMapping
     public List<User> index(@RequestParam(value = "page", defaultValue = "0") int page,
                             @RequestParam(value = "limit", defaultValue = "20") int limit) {
-        if (--page < 0) { page = 0; }
+        if (--page < 0) {
+            page = 0;
+        }
         PageRequest request = PageRequest.of(page, limit, Sort.Direction.DESC, "id");
 
         return userRepository.findAll(request).getContent();
     }
 
     @PostMapping
-    public void create(@Valid @RequestBody User user) {
+    public User create(@Valid @RequestBody User user) {
         user.setCreatedTime(LocalDateTime.now());
         user.setUpdatedTime(LocalDateTime.now());
         user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
 
-        userRepository.save(user);
+        return userRepository.save(user);
+    }
+
+    @PutMapping(path = "/{id}")
+    public User update(@PathVariable Long id, @Valid @RequestBody User user) {
+        Optional<User> existing= userRepository.findById(id);
+        if (existing == null) { return new User(); }
+
+        User originUser = existing.get();
+        originUser.setName(user.getName());
+        originUser.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        originUser.setUpdatedTime(LocalDateTime.now());
+        originUser.setEmail(user.getEmail());
+
+        userRepository.save(originUser);
+
+        return originUser;
+    }
+
+    @PatchMapping(path = "/{id}")
+    public User partialUpdate(@PathVariable Long id, @RequestBody Map<String, Object> updates) {
+        // TODO: implement partial update via reflection
+        // Rails is great!!
+        return userRepository.findById(id).get();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<User> show(@PathVariable Long id) {
+        return userRepository.findById(id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public Optional<User> destroy(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user != null) {
+            userRepository.delete(user.get());
+        }
+        return user;
     }
 }
